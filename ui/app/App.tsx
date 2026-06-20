a8e603284ded6c62d42d31173c7f81f36e4a5253
arjunmalik09 <arjun.malikcd.cse14@iitbhu.ac.in>
2026-06-20 13:26:01 +0530

    move to react community cli

--- ui/app/App.tsx
+++ ui/app/App.tsx
@@ -1,36 +1,74 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-export default function App() {
-  const [ noteTitle, setNoteTitle ] = useState('');
-  const [ noteValue, setNoteValue ] = useState('');
+import { NewAppScreen } from '@react-native/new-app-screen';
+import {
+  Alert,
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
   return (
     <View style={styles.container}>
-      <Button title='Back' />
-      <Button title='Options'/>
-      <View>
-        <TextInput
-          placeholder="New Note Title"
-          onChangeText={(newText: string) => setNoteTitle(newText)}
-          defaultValue={noteTitle}
-        />
-        <TextInput
-          placeholder="Start here..."
-          onChangeText={(newText: string) => setNoteValue(newText)}
-          defaultValue={noteValue}
-        />
-      </View>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>New Note Title</Text>
+      <TextInput placeholder="Add a new note" style={styles.input} /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  title: {
+    paddingTop: 50,
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    margin: 10,
+    width: '80%',
+    height: '80%',
+    display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
